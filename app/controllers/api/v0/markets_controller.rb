bd9696bdf73b950a080884499fdb9940976dec04
Anthea Yur <acyur6@gmail.com>
2023-12-04 20:21:08 -0600

    Made changes to use the pgdump, moving toward Factory Bot and Faker

---
+++ app/controllers/api/v0/markets_controller.rb
@@ -0,0 +1,6 @@
+class Api::V0::MarketsController < ApplicationController
+  def index
+    render json: Market.all
+  end
+
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,6 @@
 require 'simplecov'
 SimpleCov.start
+
+# require File.expand_path('../config/environment', __dir__)
+# load Rails.root.join('db', 'seeds.rb').to_s
+
