957edbe0d78ba03ac210f4dc73408d05bab8d478
Anthea Yur <acyur6@gmail.com>
2023-12-05 00:31:17 -0600

    Add serializer for Vendor

---
+++ app/serializers/vendor_serializer.rb
@@ -0,0 +1,14 @@
+class VendorSerializer
+  def self.format_vendors(vendors)
+    vendors.map do |vendor|
+      {
+        id: vendor.id,
+        name: vendor.name,
+        description: vendor.description,
+        contact_name: vendor.contact_name,
+        contact_phone: vendor.contact_phone,
+        credit_accepted: vendor.credit_accepted,
+      }
+    end
+  end
+end
