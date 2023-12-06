5d0e8fd8adb286d82ba6359d076203b0d4fa3169
Anthea Yur <acyur6@gmail.com>
2023-12-05 23:57:53 -0600

    Create Vendor Controller

---
+++ spec/requests/api/v0/vendors_request_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe "Vendors API" do
+  describe "Get All Vendors for a Market" do
+    it "can return a list of the vendors that belongs to that specific merchant" do
+      market1 = create(:market)
+      vendor1 = create(:vendor)
+      vendor2 = create(:vendor)
+      vendor3 = create(:vendor)
+    
+      MarketVendor.create(market_id:market1.id, vendor_id:vendor1.id)
+      MarketVendor.create(market_id:market1.id, vendor_id:vendor2.id)
+      MarketVendor.create(market_id:market1.id, vendor_id:vendor3.id)
+    
+      get "/api/v0/markets/#{market1.id}/vendors"
+
+    end
+
+  end
+end
