1c1f656604a8f43f9e765181e130c4b40391274f
Anthea Yur <acyur6@gmail.com>
2023-12-05 23:34:19 -0600

    Add status: 404 error

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -4,6 +4,12 @@ class Api::V0::MarketsController < ApplicationController
   end
 
   def show
-    render json: MarketSerializer.new(Market.find(params[:id]))
+    market = Market.find_by(id: params[:id])
+
+    if market
+      render json: MarketSerializer.new(Market.find(params[:id]))
+    else
+      render json: { status: 404, errors: [{ detail: "Couldn't find Market with 'id'=#{params[:id]}" }] }
+    end
   end
 end
--- spec/requests/api/v0/markets_request_spec.rb
+++ spec/requests/api/v0/markets_request_spec.rb
@@ -58,7 +58,7 @@ describe "Markets API" do
   end
 
   describe 'Get One Market' do
-    it "can get one market by its id" do
+    it "can get one market by its id (happy path)" do
       id = create(:market).id
       get "/api/v0/markets/#{id}"
       market = JSON.parse(response.body, symbolize_names: true)
@@ -67,34 +67,41 @@ describe "Markets API" do
       expect(response).to be_successful
 
       expect(market).to have_key(:id)
-        expect(market[:id]).to be_an(String)
+      expect(market[:id]).to be_an(String)
 
-        market = market[:attributes]
+      market = market[:attributes]
 
-        expect(market).to have_key(:name)
-        expect(market[:name]).to be_a(String)
-  
-        expect(market).to have_key(:street)
-        expect(market[:street]).to be_a(String)
-  
-        expect(market).to have_key(:city)
-        expect(market[:city]).to be_a(String)
-  
-        expect(market).to have_key(:county)
-        expect(market[:county]).to be_a(String)
-  
-        expect(market).to have_key(:state)
-        expect(market[:state]).to be_a(String)
-  
-        expect(market).to have_key(:state)
-        expect(market[:state]).to be_a(String)
-  
-        expect(market).to have_key(:lat)
-        expect(market[:lat]).to be_a(String)
-  
-        expect(market).to have_key(:lon)
-        expect(market[:lon]).to be_a(String)
+      expect(market).to have_key(:name)
+      expect(market[:name]).to be_a(String)
 
+      expect(market).to have_key(:street)
+      expect(market[:street]).to be_a(String)
+
+      expect(market).to have_key(:city)
+      expect(market[:city]).to be_a(String)
+
+      expect(market).to have_key(:county)
+      expect(market[:county]).to be_a(String)
+
+      expect(market).to have_key(:state)
+      expect(market[:state]).to be_a(String)
+
+      expect(market).to have_key(:state)
+      expect(market[:state]).to be_a(String)
+
+      expect(market).to have_key(:lat)
+      expect(market[:lat]).to be_a(String)
+
+      expect(market).to have_key(:lon)
+      expect(market[:lon]).to be_a(String)
+
+      expect(market).to have_key(:vendor_count)
+      expect(market[:vendor_count]).to be_a(Integer)
+    end
+
+    it "return an error status: 404 (sad path)" do
+      get "/api/v0/markets/123123123123"
+      
     end
   end
 
