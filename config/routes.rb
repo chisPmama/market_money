e1aea2e8edfde4cbd1c1d48461fd73dcf92e8d63
Anthea Yur <acyur6@gmail.com>
2023-12-07 23:27:03 -0600

    Include gem Faraday

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       get 'markets/search', to: 'markets#search'
       resources :markets, only: [:index, :show] do
         resources :vendors, only: [:index]
+        get '/nearest_atms', to: 'markets#search'
       end
       resources :vendors, only: [:show, :create, :update, :destroy]
       resources :market_vendors, only: [:create, :destroy]
