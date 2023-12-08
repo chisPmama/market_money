b41d70b78484ac0ba48d8164d781ca9a0b0c0795
Anthea Yur <acyur6@gmail.com>
2023-12-08 01:09:29 -0600

    Comment out testing for nearest atm for now

--- spec/requests/api/v0/nearest_atms_request_spec.rb
+++ spec/requests/api/v0/nearest_atms_request_spec.rb
@@ -1,27 +1,27 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-describe "Get Cash Dispensers Near a Market" do
-  before :each do
-    market_params = ({
-                      name: "Nob Hill Growers' Market",
-                      street: "Lead & Morningside SE",
-                      city: "Albuquerque",
-                      county: "Bernalillo",
-                      state: "New Mexico",
-                      zip: "null",
-                      lat: "35.077529",
-                      lon: "-106.600449"
-                    })
-    headers = {"CONTENT_TYPE" => "application/json"}
-    @market = Market.create!(market_params)
-    get "/api/v0/markets/#{@market.id}/nearest_atms", headers: headers
-  end
+# describe "Get Cash Dispensers Near a Market" do
+#   before :each do
+#     market_params = ({
+#                       name: "Nob Hill Growers' Market",
+#                       street: "Lead & Morningside SE",
+#                       city: "Albuquerque",
+#                       county: "Bernalillo",
+#                       state: "New Mexico",
+#                       zip: "null",
+#                       lat: "35.077529",
+#                       lon: "-106.600449"
+#                     })
+#     headers = {"CONTENT_TYPE" => "application/json"}
+#     @market = Market.create!(market_params)
+#     get "/api/v0/markets/#{@market.id}/nearest_atms", headers: headers
+#   end
 
-  xit "can return the closest ATM to the market" do
-    expect(response).to be_successful
-    expect(response.status).to eq(200)
+#   xit "can return the closest ATM to the market" do
+#     expect(response).to be_successful
+#     expect(response.status).to eq(200)
 
-    atms = JSON.parse(response.body, symbolize_names: true)[:data]
-    # binding.pry
-  end
-end
+#     atms = JSON.parse(response.body, symbolize_names: true)[:data]
+#     # binding.pry
+#   end
+# end
